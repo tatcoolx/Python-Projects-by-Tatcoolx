cdcd22e1890d373a999311d833a946640d4d5caa
Tatcoolx <tatcoolx@gmail.com>
2014-03-31 10:19:55 -0700

    Update B-SH.py

--- B-SH.py
+++ B-SH.py
@@ -1,6 +1,7 @@
 #-*-coding: UTF-8-*-
-import random
+import random,string
 #03.set variables
+exit=False
 draw=False
 width_min=2
 width_max=5
@@ -11,16 +12,21 @@ for i in range(65,91):
 for i in range(48,58):
     digits+=chr(i)
 name_error=True
-
-
-
+shot_out=[0,0,0]
+shot_miss=[0,0,0]
+shot_dead=[0,0,0]
+shot_kill=[0,0,0]
 #014. takes all digits in str and stacks them.
 def str_to_int(a):
+    if len(str(a))==0:
+        print "error, in str_to_int, length=0"
     str_to_int_output=""
     for i in range(len(a)):
         for b in range(10):
             if a[i]==str(b):
                 str_to_int_output+=a[i]
+    if len(str(str_to_int_output))==0:
+        str_to_int_output=0
     return int(str_to_int_output)
 #22. Random name for bship
 def ship_id_gen():
@@ -29,22 +35,42 @@ def ship_id_gen():
     id+="-"
     id+=random.choice(digits)
     return id
+#30. statistic
+def statistic():
+    print
+    print ("*"*18)+"!STATISTIC!"+("*"*18)
+    print
+    print "Name:"(" "*15)+"  "+player_st[1]+"  "+player_st[2]
+    print "Total Shots:          "+str(turn/2+1)+(" "*(17-len(str(turn/2+1))))+str(turn/2)+(" "*(15-len(str(turn/2))))
+    print "Missed Shots:         "+str(shot_miss[1])++(" "*(17-len(str(shot_miss[1]))))+str(shot_miss[2])+(" "*(15-len(str(shot_miss[2]))))
+    print "Outside of ocean:     "+str(shot_out[1])++(" "*(17-len(str(shot_out[1]))))+str(shot_out[2])+(" "*(15-len(str(shot_out[2]))))
+    print "Corpse shots:         "+str(shot_dead[1])++(" "*(17-len(str(shot_dead[1]))))+str(shot_dead[2])+(" "*(15-len(str(shot_dead[2]))))
+    print "Battleships destroyed:"+str(shot_kill[1])++(" "*(17-len(str(shot_kill[1]))))+str(shot_kill[2])+(" "*(15-len(str(shot_kill[2]))))
+    print "Scores:               "+str(scores[1])++(" "*(17-len(str(scores[1]))))+str(scores[2])+(" "*(15-len(str(scores[2]))))
 #29.welcoming
 print
 print "Let's play Battleship!"
 print
-#33.players list, empty
+#33.players lists, empty
 player=["AI"]
+player_st=["AI"]
 maxscore_owner=0
 #36.set players name 
 for i in range(2):
-    name=str(raw_input("Enter name for Player "+str(i+1)+":"))
+    name=str(raw_input("Enter name for Player "+str(i+1)+"(max 15):"))
     if len(name)==0:
         name="Player "+str(i+1)
+    elif len(name)>15:
+        name=name[:15]
     player.append(name)
+    player_st.append(name)
     print
     print "Welcome "+name+"!!!"
     print
+for i in range(1,3):
+    for x in range (15-len(player_st[i])):
+        player_st[i]+=" "
+print player_st
 #45.field as list of lists, empty
 board = []
 #47.set width for field and fill it 
@@ -178,22 +204,51 @@ while winscore>maxscore:
     print
     raw_input("Turn "+str(turn)+" by "+str(player[currentplayer])+", press ENTER.")
     print_board(board)
-    guess_row = int(raw_input("Guess Row:"))-1
-    guess_col = int(raw_input("Guess Col:"))-1
+    while True:
+        guess_row=raw_input("Guess Row:")
+        guess_col=raw_input("Guess Col:")
+        if string.lower(guess_row)=="stat" or string.lower(guess_col)=="stat":
+    		statistic()
+        elif string.lower(guess_row)=="exit" or string.lower(guess_col)=="exit":
+            exit=True
+            break
+        else:
+			if len(guess_row)==0:
+				guess_row=random.randint(1,width)-1
+			else:
+				guess_row=str_to_int(guess_row)-1
+			if len(guess_col)==0:
+				guess_col=random.randint(1,width)-1
+			else:
+				guess_col=str_to_int(guess_col)-1
+			print guess_row
+			print guess_col
+			break
+    if exit==True:
+        break
     for i in range(15):
         print
     if (guess_row < 0 or guess_row > width-1) or (guess_col < 0 or guess_col > width-1):
         print_board(board)
         raw_input("Oops, that's not even in the ocean.")
+        shot_out[currentplayer]+=1
     elif board[guess_row][guess_col] == "X":
         print_board(board)
-        raw_input("You guessed that one already. Times shooted: "+coor_status[str(guess_row+1)+str(guess_col+1)][1:]+".")
+        coor_status[str(guess_row+1)+str(guess_col+1)]="X"+str(int(coor_status[str(guess_row+1)+str(guess_col+1)][1:])+1)
+        raw_input("Guessed that one already. Times shooted: "+coor_status[str(guess_row+1)+str(guess_col+1)][1:]+".")
+        shot_miss[currentplayer]+=1
+    elif coor_status[str(guess_row+1)+str(guess_col+1)][:4]=="dead":
+        coor_status[str(guess_row+1)+str(guess_col+1)]="X"+str(int(coor_status[str(guess_row+1)+str(guess_col+1)][4:])+1)
+        raw_input("Destroyed that one already. Times shooted: "+coor_status[str(guess_row+1)+str(guess_col+1)][4:]+".")
+        shot_dead[currentplayer]+=1
     elif coor_status[str(guess_row+1)+str(guess_col+1)][:5]=="alive":
         board[guess_row][guess_col] = "$"
         print_board(board)
-        print "You sunk the battleship "+ship_id[coor_status[str(guess_row+1)+str(guess_col+1)][5:len(coor_status[str(guess_row+1)+str(guess_col+1)])]]+"!"
+        print "You sunk the battleship "+ships_id[int(coor_status[str(guess_row+1)+str(guess_col+1)][5:])]+"!"
         scores[currentplayer]+=1
+        coor_status[str(guess_row+1)+str(guess_col+1)]="dead1"
         raw_input(str(player[currentplayer])+" got +1 score.")
+        shot_kill[currentplayer]+=1
         ships_dead-=1
         if maxscore<scores[currentplayer]:
             maxscore=scores[currentplayer]
@@ -208,6 +263,7 @@ while winscore>maxscore:
         coor_status[str(guess_row+1)+str(guess_col+1)]="X1"
         print_board(board)
         raw_input("You missed")
+        shot_miss[currentplayer]+=1
     print
     for i in range(1,3):
         print str(player[i])+": "+str(scores[i])+" scores"
@@ -219,14 +275,17 @@ while winscore>maxscore:
     print 
     raw_input("Press ENTERT to continue...")
     print ("*"*40)
-#186. Victory and draw text
+#186. Exit or Victory + draw text
 for i in range(20):
     print
-if draw==True:
-    raw_input("Not BAD and not GOOD but DRAW!!!")
-else:
-    print ("*"*15)+"!CONGRATULATIONS!"+("*"*15)
-    print
-    print str(player[maxscore_owner])+" has won!!!!"
-    print
-    raw_input(str(player[3-maxscore_owner])+" is FUCKING LOSER!!!!!!1111")
+if exit==False:
+	if draw==True:
+		raw_input("Not BAD and not GOOD but DRAW!!!")
+	else:
+		print ("*"*15)+"!CONGRATULATIONS!"+("*"*15)
+		print
+		print str(player[maxscore_owner])+" has won!!!!"
+		print
+		raw_input(str(player[3-maxscore_owner])+" is FUCKING LOSER!!!!!!1111")
+		statistic()
+raw_input("Thanks for playing! Goodbye!")
